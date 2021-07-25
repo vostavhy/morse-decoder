c91aee03538dee96cba34f7abaac3ffde4329bc8
vostavhy <vostavhy@gmail.com>
2021-07-25 18:09:27 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -38,9 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+
+    while (expr) {
+        result += code_to_char(expr.substring(0, 10));
+        expr = expr.substring(10)        
+    }
+
+    return result;
 }
 
 module.exports = {
     decode
+}
+
+function remove_zero(str) {
+    if (str[0] != '0') return str;
+    return remove_zero(str.substring(1));
+}
+
+function code_to_char(int_code) {
+    if (int_code[0] == '*') return ' ';
+
+    let morse_code = '';
+    int_code = remove_zero(int_code);
+
+    while (int_code) {
+        let number = int_code.substring(0, 2);
+        if (number == '10') morse_code += '.';
+        else if (number == '11') morse_code += '-';
+        int_code = int_code.substring(2);
+    }
+    return MORSE_TABLE[morse_code];
 }
